0a4f79a18b1a20db29cb91b4ffbbe3b3b98f42ab
AlistairGJ <AlistairGJ@gmail.com>
2019-08-10 02:38:53 +1000

    added venv for Py27 + cleaned up py37 code

---
+++ py27/cana.py
@@ -0,0 +1,14 @@
+
+from __future__ import division
+import os
+import math
+import numpy as np
+import pandas as pd
+pd.options.display.float_format = '{:.2g}'.format
+import graphviz
+import cana
+
+import cana
+from cana.drawing import draw_canalizing_map_graphviz
+from cana.drawing import draw_dynamics_canalization_map_graphviz
+
