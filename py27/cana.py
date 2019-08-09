421f33ba5c873dfa2f691ccf2ca45abfcba39d05
AlistairGJ <AlistairGJ@gmail.com>
2019-08-10 03:22:07 +1000

    update

--- py27/cana.py
+++ py27/cana.py
@@ -1,14 +1,18 @@
 
 from __future__ import division
-import os
-import math
-import numpy as np
-import pandas as pd
-pd.options.display.float_format = '{:.2g}'.format
-import graphviz
-import cana
+#import numpy as np
+#from itertools import product
+#from cana.control import fvs, mds, sc
+from cana.datasets.bio import THALIANA
+from IPython.display import display
+from IPython.core.display import HTML
 
-import cana
-from cana.drawing import draw_canalizing_map_graphviz
-from cana.drawing import draw_dynamics_canalization_map_graphviz
+N = THALIANA()
 
+display(HTML('<h3>Control State Transition Graph (CSTG)</h3>'))
+# THIS MIGHT TAKE A LONG TIME, it is here for demo purposes.
+driver_nodes = N.attractor_driver_nodes(min_dvs=1, max_dvs=6, verbose=True)
+print N.get_node_name(driver_nodes)
+
+
+# The FREQUENCY that two events has a certain temporal relationship
